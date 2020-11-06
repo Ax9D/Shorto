624007efbfa2061c576851ab09f1b58ef4a52ab8
atri <atrisarkar91@gmail.com>
2020-11-07 00:11:52 +0530

    updated readme

---
+++ common/common/errors.js
@@ -0,0 +1,30 @@
+module.exports = {
+    INVALID_URL: {
+        code: 111,
+        message: "Invalid URL"
+    },
+    INVALID_ANALYTICS_ID: {
+        code: 123,
+        message: "Invalid analytics ID"
+    },
+    UNKNOWN_SHORT_ID: {
+        code: 234,
+        message: "Unknown shortID"
+    },
+    NO_ANALYTICS_ID:{
+        code: 999,
+        message:"No analytics ID provided"
+    },
+    CONNECTION_TIMED_OUT:{
+        code:69420,
+        message:"Connection timed out"
+    },
+    BAD_CAPTCHA:{
+        code:1002,
+        message:"Couldn't verify captcha"
+    },
+    INTERNAL_SERVER_ERROR:{
+        code:001,
+        message:"Internal server error"
+    }
+};
---
+++ common/errors.js
@@ -0,0 +1,22 @@
+module.exports = {
+    INVALID_URL: {
+        code: 111,
+        message: "Invalid URL"
+    },
+    INVALID_ANALYTICS_ID: {
+        code: 123,
+        message: "Invalid analytics ID"
+    },
+    UNKNOWN_SHORT_ID: {
+        code: 234,
+        message: "Unknown shortID"
+    },
+    NO_ANALYTICS_ID:{
+        code: 999,
+        message:"No analytics ID provided"
+    },
+    CONNECTION_TIMED_OUT:{
+        code:69420,
+        message:"Connection timed out"
+    }
+};
